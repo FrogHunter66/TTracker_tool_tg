a49ee28b208b90fa7943712a7f2f55810447aec0
Nikita <nikita.fdorov99@mail.ru>
2024-02-26 17:06:38 +0300

    Add new func to app.py file

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+
+async def super_important_func():
+    print("Hello World")
+
+
 async def on_startup(dp):
     print("Bot started")
