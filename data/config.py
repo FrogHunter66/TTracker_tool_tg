eb6f280c179ada969b0bd77030f77c4b7198b64e
Nikita <nikita.fdorov99@mail.ru>
2024-02-26 17:28:54 +0300

    Add changes to files

--- data/config.py
+++ data/config.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+DATABASE_URI = ""
 BOT_TOKEN = str(os.getenv("TOKEN"))
--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-
+import math
+import time
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.MARKDOWN)
